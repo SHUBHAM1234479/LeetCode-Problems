2189351fb9fa430dc627d6118ecbe385186b263c
Shubham Birajdar <64602513+SHUBHAM1234479@users.noreply.github.com>
2025-08-04 20:04:05 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class Palindrome  {
+    public boolean isPalindrome(int x) {
+        int rev = 0;
+        for(int i = x;i>0;i/=10)
+        {
+            rev= (rev*10)+ i%10;
+        }
+        System.out.println(rev) ;
+        if(rev == x){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+
+    public static void main(String args[]){
+
+        Palindrome s = new Palindrome();
+        System.out.println("Enter no To Check Palindrome") ;
+        int num = new Scanner(System.in).nextInt() ;
+
+        System.out.println(s.isPalindrome(num)) ;
+    }
+}
